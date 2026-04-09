c262180cd4ff9aefa0bf207df6aef808c3435a49
Rosieeee344 <rboahemaa04@gmail.com>
2026-04-09 13:36:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const name = "Dwamena Sarkodie Boahemaa Rosemary";
+const age = 23;
+const favoritelanguage = "JavaScript";
+
+console.log('Name: ', name);
+console.log('Age: ', age);
+console.log('Favorite Programming Language: ', favoritelanguage);
